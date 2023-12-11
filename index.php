b01f1462592b8b9c5a79d67333b491af369fa775
ALEX4891 <alexx4891@mail.ru>
2023-12-11 18:55:56 +0500

    первый коммит

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<?
-    $files = glob("./photos/*.{jpg,png,jpeg,png}", GLOB_BRACE);
-    if (isset($_POST['copyImage'])) {    
-            foreach($files as $file) {
-                $dirname    = dirname( $file ).DIRECTORY_SEPARATOR;
-                $filename   = pathinfo( $file, PATHINFO_FILENAME );
-                $extension  = pathinfo( $file, PATHINFO_EXTENSION );
-                 copy($file, $dirname . 'new_' . $filename . '.' . $extension);
-        }
-        header('location: /');
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,24 +7,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-        <div class="container" style="display: flex; justify-content: center; flex-direction: row; flex-wrap: wrap; gap: 10px;">
+        <!-- <div class="container" style="display: flex; justify-content: center; flex-direction: row; flex-wrap: wrap; gap: 10px;"> -->
     <? 
-        foreach($files as $key => $file) {
-            if ($key != 0 && $key % 4 == 0) {
-                echo '<br>';
+        $nums = range(1,10);
+
+        // первая строка
+        echo " <table>";
+        for ($i = 1; $i <= 10; $i++)
+        {
+            echo "";
+            for ($j = 1; $j <= 10; $j++)
+            {
+                $result = $i * $j;
+                echo "";
             }
-            echo '<img width="250" height="250" src=' . $file . ' alt=' . $file . ' >';
+            echo "";
         }
-    ?>
-        </div>;
-
-        <div class="container d-flex justify-content-center">
-            <form action="#" method="POST">
-                <input class="btn btn-primary" name="copyImage" type="submit" value="Скопировать изображения" />
-            </form>
-        </div>
-
-    
+        echo "
+        $i * $j = $result
+        ";
+    ?>   
 
 </body>
 </html>
